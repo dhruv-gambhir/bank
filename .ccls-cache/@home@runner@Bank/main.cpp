0a38bfe9c9e5e454d50901da723d9eb797eeaa57
Dhruv Gambhir <34659471-gambhirdhruv03@users.noreply.replit.com>
2024-07-02 03:22:00 +0000

    initial

--- .ccls-cache/@home@runner@Bank/main.cpp
+++ .ccls-cache/@home@runner@Bank/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Welcome to the Bank\n Please Enter your Account Number\n Press n to open an account\n Press e to exit\n";
+  std::cout << "Welcome to the Bank\n Please Enter your Account Number\n Press "
+               "n to open an account\n Press e to exit\n";
   std::string response;
-  std::cin>>response;
-  switch(response){
-    case "n":
-      std::cout<<"Opening an account";
-      break;
-    case "e":
-      std::cout<<"Exiting";
-      break;
-    default:
-      checkAccount();
+  std::cin >> response;
+  if (response == 'n') {
+
+    std::cout << "Opening an account";
+  } else if {
+    std::cout << "Exiting";
+  } else {
+    checkAccount();
   }
+}
 }
